f1626f0e17db98aeb5c530441701f34fb29c3247
Nikita <wolf24ru@gmail.com>
2022-03-09 17:45:44 +0700

    client

--- client.py
+++ client.py
@@ -16,27 +16,27 @@ print(response)
 
 
 # get user fo id
-get_user = requests.get(f'{URL}get_user/1')
-print(get_user)
-print(f'get_user: {get_user.json()}')
+# get_user = requests.get(f'{URL}get_user/1')
+# print(get_user)
+# print(f'get_user: {get_user.json()}')
 
 # create advertisement
-# create_advertisement = requests.post(f'{URL}adv_new',
-#                                      json={
-#                                          'title': 'thing_2',
-#                                          'description': 'lzlzl',
-#                                          'user_name': 'user_3',
-#                                          'password': '12333Sdf545sdf',
-#                                      })
-# print('____create_advertisement____')
-# print(create_advertisement)
-# print(create_advertisement.json())
+create_advertisement = requests.post(f'{URL}adv_new',
+                                     json={
+                                         'title': 'thing_6',
+                                         'description': 'ttu',
+                                         'user_name': 'user_2',
+                                         'password': '12333Sdf545sdf',
+                                     })
+print('____create_advertisement____')
+print(create_advertisement)
+print(create_advertisement.json())
 
-# get_advertisement_all = requests.get(f'{URL}adv')
-# print('\n____get_advertisement_all____')
-# print(get_advertisement_all)
-# print(get_advertisement_all.json())
-#
+get_advertisement_all = requests.get(f'{URL}adv')
+print('\n____get_advertisement_all____')
+print(get_advertisement_all)
+print(get_advertisement_all.json())
+
 #
 # get_advertisement_1 = requests.get(f'{URL}adv/1')
 # print('\n____get_advertisement_1____')
@@ -52,12 +52,12 @@ print(f'get_user: {get_user.json()}')
 # print(del_advertisement_1)
 # print(del_advertisement_1.json())
 
-# patch_advertisement_2 = requests.patch(f'{URL}adv_patch/1',
+# patch_advertisement_2 = requests.patch(f'{URL}adv_patch/2',
 #                                        json={
-#                                            'user_name': f'user_3',
+#                                            'user_name': f'user_1',
 #                                            'password': '12333Sdf545sdf',
 #                                            'title': '4rfff'
 #                                        })
-# print('\n____get_advertisement_1____')
+# print('\n____patch_advertisement_1____')
 # print(patch_advertisement_2)
 # print(patch_advertisement_2.json())
